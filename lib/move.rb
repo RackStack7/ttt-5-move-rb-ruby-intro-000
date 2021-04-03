313589ae4a8bffdc5fca2018fe043708785f5412
James Daaran <jamesdaaran@gmail.com>
2021-04-03 15:54:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,15 +1,9 @@
-def move
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(input)
-input_to_index = input.to_i
-index = input_to_index - 1
 end
 
 def update_array_at_with(board, index, value)
